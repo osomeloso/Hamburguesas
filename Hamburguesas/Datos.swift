51cbf1e945928831abceb7ab7f64e47530e9f0e5
osomeloso <oso_meloso@hotmail.com>
2016-03-27 15:54:54 -0430

    Implementación de Clases Países y Hamburguesas

--- Hamburguesas/Datos.swift
+++ Hamburguesas/Datos.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+
+class ColeccionDePaises {
+    let países = ["Alemanania",
+                "Argentina",
+                "Bolivia",
+                "Brasil",
+                "Canadá",
+                "Chile",
+                "Colombia",
+                "Costa Rica",
+                "Ecuador",
+                "El Salvador",
+                "España",
+                "Estados Unidos",
+                "Francia",
+                "Guatemala",
+                "Honduras",
+                "Italia",
+                "Mexico",
+                "Nicaragua",
+                "Panamá",
+                "Paraguay",
+                "Portugal",
+                "Peru",
+                "Uruguay",
+                "Venezuela"]
+    
+    func obtenPais() -> String {
+        let posicion = Int(arc4random()) % países.count
+        return países[posicion]
+    }
+}
+
+class ColeccionDeHamburguesa {
+    let hamburguesas = ["Alemana",
+                   "Pelusa",
+                   "Vegetariana",
+                   "Carioca",
+                   "Justin Bieber",
+                   "Puerto Montt",
+                   "Cumbia",
+                   "Pura Vida",
+                   "King Kong",
+                   "Marilin Monroe",
+                   "Toro Rojo",
+                   "Elvis Presley",
+                   "4 quesos",
+                   "Quetzalcoatl",
+                   "Titicaca",
+                   "Siciliana",
+                   "Pancho Villa",
+                   "Nicaragua",
+                   "Moby Dick",
+                   "Inca",
+                   "Media libra",
+                   "Llanera"]
+    
+    func obtenHamburguesa() -> String {
+        let posicion = Int(arc4random()) % hamburguesas.count
+        return hamburguesas[posicion]
+    }
+}
